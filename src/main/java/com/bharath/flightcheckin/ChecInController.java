84165f8565951eb348dbe6928e4373e66984cdc1
Anthony Albert Siao <tonysiao27@gmail.com>
2018-05-18 10:28:13 +0800

    Initial commit

---
+++ src/main/java/com/bharath/flightcheckin/ChecInController.java
@@ -0,0 +1,43 @@
+package com.bharath.flightcheckin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.bharath.flightcheckin.integration.ReservationRestClient;
+import com.bharath.flightcheckin.integration.dto.Reservation;
+import com.bharath.flightcheckin.integration.dto.ReservationUpdateRequest;
+
+@Controller
+public class ChecInController {
+
+	@Autowired
+	ReservationRestClient restClient;
+
+	@RequestMapping("/showStartCheckin")
+	public String showStartCheckIn() {
+		return "startCheckIn";
+	}
+
+	@RequestMapping("/startCheckIn")
+	public String startCheckIn(@RequestParam("reservationId") Long reservationId, ModelMap modelMap) {
+		Reservation reservation = restClient.findReservation(reservationId);
+		modelMap.addAttribute("reservation", reservation);
+		return "displayReservationDetails";
+	}
+
+	@RequestMapping("/completeCheckIn")
+	public String completeCheckIn(@RequestParam("reservationId") Long reservationId,
+			@RequestParam("numberOfBags") int numberOfBags) {
+		ReservationUpdateRequest reservationUpdateRequest = new ReservationUpdateRequest();
+		reservationUpdateRequest.setId(reservationId);
+		reservationUpdateRequest.setCheckedIn(true);
+		reservationUpdateRequest.setNumberOfBags(numberOfBags);
+		restClient.updateReservation(reservationUpdateRequest);
+		return "checkInConfirmation";
+
+	}
+
+}
